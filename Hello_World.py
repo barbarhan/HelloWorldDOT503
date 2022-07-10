e69b338157b818c7f99ca2f8eeb25b1f52937185
Baris Albayrak <80099286+barbarhan@users.noreply.github.com>
2022-07-10 18:12:10 +1000

    Update Hello_World.py

--- Hello_World.py
+++ Hello_World.py
@@ -4,3 +4,21 @@
 print('Hello, World!')
 
 
+print('Hello, World!')
+
+print('What did you eat today?')
+a = input()
+print('Glad to hear that sounds delicious, bon apetit')
+=======
+#!/usr/bin/env python
+# coding: utf-8
+
+#Baris Albayrak
+
+#Hello World DevOps A2
+
+
+print('Hello, World!')
+
+x = input('Enter your age:')
+print('Hello, ',x , 'years old mate. I wish you a good day.')
